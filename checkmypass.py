20c57192c4071ebf65df82dc30949b8616ad8f00
vicenvafer <vicenvafer006@gmail.com>
2025-08-29 14:04:34 +0100

    defined functions
    password is encoded and everything all thats left is sending it to the api

--- checkmypass.py
+++ checkmypass.py
@@ -1,20 +1,21 @@
 import requests
+import hashlib
 
-#print(requests.__file__)
-
-url = "https://api.pwnedpasswords.com/range/" + "password123"
-
-res = requests.get(url)
-
-print(res)
 
 def request_api_data(query_char):
-    pass
+    url = "https://api.pwnedpasswords.com/range/" + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f"Error fetching {res.status_code}, check API and try again.")
+    return res
+
 
 
 def pwned_api_check(password):
-    #check is passwrd exists in api response
-    pass
+    sha1password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
+    return sha1password
+
+
 
 
 
